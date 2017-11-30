b37a8480e6f26828f10e08ad42274fa6a26e6e97
LUTECH-TMS-CERT <L-TMS-CERT@users.noreply.github.com>
2017-11-30 16:36:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'eyeonthreat',
   packages = ['eyeonthreat'],
-  version = '1.0',
+  version = '1.1',
   description = 'EyeOnThreat provides Feed or API access to a relevant part of Cyber Threat Intelligence Information gathered, analyzed and released by Lutech and multiple open and private sources',
   author = 'Lutech Cyber Threat Intelligence Team',
   author_email = 'supporto.security-intelligence@lutech.it',
